02ae9aed7b5153eb5603927d9db01ebab30198fa
Sébastien Sirtoli <sebastien.sirtoli@affinitic.be>
2016-12-10 21:56:48 +0100

    Add Docker File, Makefile and configuration

---
+++ alembic/versions/9c5fd253fb88_first.py
@@ -0,0 +1,24 @@
+"""first
+
+Revision ID: 9c5fd253fb88
+Revises: 
+Create Date: 2016-12-10 20:55:37.995737
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9c5fd253fb88'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
